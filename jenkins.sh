88fcbc475a1664a23ad410821e90fa7a84ede14a
ramki2537 <49984662+ramki2537@users.noreply.github.com>
2025-06-08 14:37:30 +0800

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -2,21 +2,21 @@
 #!/bin/bash
 
 #resize disk from 20GB to 50GB
-growpart /dev/nvme0n1 4
+sudo growpart /dev/nvme0n1 4
 
-lvextend -L +10G /dev/RootVG/rootVol
-lvextend -L +10G /dev/mapper/RootVG-varVol
-lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
+sudo lvextend -L +10G /dev/RootVG/rootVol
+sudo lvextend -L +10G /dev/mapper/RootVG-varVol
+sudo lvextend -l +100%FREE /dev/mapper/RootVG-varTmpVol
 
-xfs_growfs /
-xfs_growfs /var/tmp
-xfs_growfs /var
+sudo xfs_growfs /
+sudo xfs_growfs /var/tmp
+sudo xfs_growfs /var
 
 
-curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum install fontconfig java-17-openjdk jenkins -y
-yum install jenkins -y
-ystemctl daemon-reload
-systemctl enable jenkins
-systemctl start jenkins
+sudo curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum install fontconfig java-17-openjdk jenkins -y
+sudo yum install jenkins -y
+sudo ystemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
